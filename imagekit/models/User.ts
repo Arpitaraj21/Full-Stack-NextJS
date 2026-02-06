d0e29c611e25d8c24d70191d1445b068af03529a
arpita0212 <arpita@pipra.solutions>
2026-02-06 18:06:14 +0530

    models for user and video

--- imagekit/models/User.ts
+++ imagekit/models/User.ts
@@ -30,7 +30,7 @@ userSchema.pre('save', async function () {
     this.password = await bcrypt.hash(this.password, 10);
 });
 
-
+//generics in typescript
 const User = models?.User || model<IUser>("User", userSchema);
 
 
